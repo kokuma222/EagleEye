5c615e3b19498aadd158c87f4c441d4d0ae69de1
kokuma222 <zhaopf123@gmail.com>
2019-08-21 19:41:03 +0800

    first try

---
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
@@ -0,0 +1,18 @@
+package com.thoughtmechanix.licenses.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Fency on 2019/8/17.
+ */
+@Component
+public class ServiceConfig {
+
+    @Value("${example.property}")
+    private String exampleProperty;
+
+    public String getExampleProperty() {
+        return exampleProperty;
+    }
+}
