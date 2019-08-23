19026e43d3e66bd95049337bd30d9d17239e04c6
kokuma222 <zhaopf123@gmail.com>
2019-08-23 08:29:58 +0800

    add consvr auto refresh

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/config/ServiceConfig.java
@@ -1,12 +1,14 @@
 package com.thoughtmechanix.licenses.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 /**
  * Created by Fency on 2019/8/17.
  */
 @Component
+@RefreshScope
 public class ServiceConfig {
 
     @Value("${example.property}")
