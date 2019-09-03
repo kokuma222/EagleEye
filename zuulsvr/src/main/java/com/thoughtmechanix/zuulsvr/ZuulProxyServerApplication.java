a369502dac8c3504bb42403139c7554165c8f13c
kokuma222 <zhaopf123@gmail.com>
2019-09-03 19:58:51 +0800

    zuul first commit

--- zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/ZuulProxyServerApplication.java
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/ZuulProxyServerApplication.java
@@ -2,12 +2,14 @@ package com.thoughtmechanix.zuulsvr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ZuulProxyServerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ZuulProxyServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulProxyServerApplication.class, args);
+    }
 
 }
