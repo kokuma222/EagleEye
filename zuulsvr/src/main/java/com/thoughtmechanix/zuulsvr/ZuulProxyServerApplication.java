29838f9c7c1b25ca9a592ac0ff8ba687597c7983
kokuma222 <zhaopf123@gmail.com>
2019-09-03 19:58:00 +0800

    zuul first commit

---
+++ zuulsvr/src/main/java/com/thoughtmechanix/zuulsvr/ZuulProxyServerApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtmechanix.zuulsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZuulProxyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulProxyServerApplication.class, args);
+	}
+
+}
