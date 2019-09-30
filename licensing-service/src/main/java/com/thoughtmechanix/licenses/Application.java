c7edaeed3a284daf3343a5a1207358bac961fdb3
kokuma222 <zhaopf123@gmail.com>
2019-09-30 20:08:23 +0800

    redis first try

--- licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/Application.java
@@ -1,9 +1,12 @@
 package com.thoughtmechanix.licenses;
 
+import com.thoughtmechanix.licenses.config.ServiceConfig;
 import com.thoughtmechanix.licenses.events.models.OrganizationChangeModel;
+import com.thoughtmechanix.licenses.model.Organization;
 import com.thoughtmechanix.licenses.utils.UserContextInterceptor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
@@ -13,6 +16,8 @@ import org.springframework.cloud.stream.annotation.EnableBinding;
 import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
@@ -27,12 +32,15 @@ import java.util.List;
 //@EnableCircuitBreaker
 @EnableBinding(Sink.class)
 public class Application {
+    @Autowired
+    private ServiceConfig serviceConfig;
+
     private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
-    @StreamListener(Sink.INPUT)
+    /*@StreamListener(Sink.INPUT)
     public void loggerSink(OrganizationChangeModel orgChange) {
         logger.warn("Received an event for organization id {}", orgChange.getOrganizationId());
-    }
+    }*/
 
     @LoadBalanced
     @Bean
@@ -45,6 +53,21 @@ public class Application {
             interceptors.add(new UserContextInterceptor());
             template.setInterceptors(interceptors);
         }
+        return template;
+    }
+
+    @Bean
+    public JedisConnectionFactory jedisConnectionFactory() {
+        JedisConnectionFactory jedisConnFactory = new JedisConnectionFactory();
+        jedisConnFactory.setHostName(serviceConfig.getRedisServer());
+        jedisConnFactory.setPort(serviceConfig.getRedisPort());
+        return jedisConnFactory;
+    }
+
+    @Bean
+    public RedisTemplate<String, Organization> redisTemplate() {
+        RedisTemplate<String, Organization> template = new RedisTemplate<>();
+        template.setConnectionFactory(jedisConnectionFactory());
         return template;
     }
 
--- licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
+++ licensing-service/src/main/java/com/thoughtmechanix/licenses/clients/OrganizationRestTemplateClient.java
@@ -1,6 +1,7 @@
 package com.thoughtmechanix.licenses.clients;
 
 import com.thoughtmechanix.licenses.model.Organization;
+import com.thoughtmechanix.licenses.repository.OrganizationRedisRepository;
 import com.thoughtmechanix.licenses.utils.UserContextHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,7 +14,10 @@ import org.springframework.web.client.RestTemplate;
 @Component
 public class OrganizationRestTemplateClient {
     @Autowired
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private OrganizationRedisRepository orgRedisRepo;
 
     private static final Logger logger = LoggerFactory.getLogger(OrganizationRestTemplateClient.class);
 
@@ -27,7 +31,7 @@ public class OrganizationRestTemplateClient {
         return restExchange.getBody();
     }*/
 
-    public Organization getOrganization(String organizationId) {
+    /*public Organization getOrganization(String organizationId) {
         logger.warn(">>> In licensing Service.getOrganization: {}. Thread Id: {}", UserContextHolder.getContext().getCorrelationId(), Thread.currentThread().getId());
 
         ResponseEntity<Organization> restExchange =
@@ -37,5 +41,50 @@ public class OrganizationRestTemplateClient {
                         null, Organization.class, organizationId);
 
         return restExchange.getBody();
+    }*/
+
+    private Organization checkRedisCache(String organizationId) {
+        try {
+            return orgRedisRepo.findOrganization(organizationId);
+        }
+        catch (Exception ex){
+            logger.error("Error encountered while trying to retrieve organization {} check Redis Cache.  Exception {}", organizationId, ex);
+            return null;
+        }
+    }
+
+    private void cacheOrganizationObject(Organization org) {
+        try {
+            orgRedisRepo.saveOrganization(org);
+        }catch (Exception ex){
+            logger.error("Unable to cache organization {} in Redis. Exception {}", org.getId(), ex);
+        }
+    }
+
+    public Organization getOrganization(String organizationId) {
+        logger.debug(">>> In licensing Service.getOrganization: {}. Thread Id: {}", UserContextHolder.getContext().getCorrelationId(), Thread.currentThread().getId());
+
+        Organization org = checkRedisCache(organizationId);
+
+        if (org != null) {
+            logger.debug("I have successfully retrieved an organization {} from the redis cache: {}", organizationId, org);
+            return org;
+        }
+
+        logger.debug("Unable to locate organization from the redis cache: {}.", organizationId);
+
+        ResponseEntity<Organization> restExchange =
+                restTemplate.exchange(
+                        "http://organizationservice/v1/organizations/{organizationId}",
+                        HttpMethod.GET,
+                        null, Organization.class, organizationId);
+
+        org = restExchange.getBody();
+
+        if (org != null) {
+            cacheOrganizationObject(org);
+        }
+
+        return org;
     }
 }
